3b1fa6319d9d98d23e8a65aa7388ab870c261a6f
Seepu-kansal <seepakansal@gmail.com>
2020-06-30 19:02:49 +0530

    Commit

--- src/test/java/com/example/talent/HireTalentApplicationTests.java
+++ src/test/java/com/example/talent/HireTalentApplicationTests.java
@@ -2,6 +2,9 @@ package com.example.talent;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.security.RunAs;
 
 @SpringBootTest
 class HireTalentApplicationTests {
